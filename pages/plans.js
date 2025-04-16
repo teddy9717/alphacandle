966cf00c7d5454c587a1e4bf9b74056764561951
teddy9717 <btsig77@gmail.com>
2025-04-16 16:57:48 +0900

    Add files via upload

---
+++ pages/plans.js
@@ -0,0 +1,16 @@
+
+export default function Plans() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '40px' }}>
+      <h1 style={{ fontSize: '36px' }}>Subscription Plans</h1>
+      <ul style={{ marginTop: '20px', fontSize: '18px' }}>
+        <li>💎 1 Month - $99</li>
+        <li>💎 3 Months - $249</li>
+        <li>💎 12 Months - $599</li>
+      </ul>
+      <p style={{ marginTop: '20px' }}>
+        Payments will be made in USDT. After payment, you will receive instant access to our private Telegram channel.
+      </p>
+    </div>
+  );
+}
